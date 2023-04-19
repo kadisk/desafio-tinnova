094482134070255ca3d895bc73cdfc4a9accbd9a
Kaio Cezar <kadisk.shark@gmail.com>
2023-04-19 20:11:16 -0300

    -

--- bubble-sort/src/main.c
+++ bubble-sort/src/main.c
@@ -2,22 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+void exibir_vetor(int vetor[], int tamanho, int i, int j) {
+    for (int k = 0; k < tamanho; k++) {
+        if (k == j) {
+            printf("(%d  %d) ", vetor[k], vetor[k + 1]);
+        } else if (k != j + 1) {
+            printf("%d  ", vetor[k]);
+        }
+    }
+    printf("\n");
+}
+
 void bubble_sort(int vetor[], int tamanho) {
     int i, j, temp;
     for (i = 0; i < tamanho - 1; i++) {
         printf("\nRodada %d\n", i + 1);
         printf("==================================================\n");
         for (j = 0; j < tamanho - 1 - i; j++) {
-            printf("(%d  %d) ", vetor[j], vetor[j + 1]);
+            exibir_vetor(vetor, tamanho, i, j);
             if (vetor[j] > vetor[j + 1]) {
                 temp = vetor[j];
                 vetor[j] = vetor[j + 1];
                 vetor[j + 1] = temp;
+                printf("%*s--", 3 * j, "");
+                exibir_vetor(vetor, tamanho, i, j);
                 printf("trocamos\n");
             } else {
                 printf("mantemos <----\n");
             }
         }
+        printf("\n");
+        for (int k = 0; k < tamanho - 1 - i; k++) {
+            printf("%d  ", vetor[k]);
+        }
+        for (int k = tamanho - 1 - i; k < tamanho; k++) {
+            printf("[%d] ", vetor[k]);
+        }
+        printf("final da rodada %d\n", i + 1);
     }
 }
 
